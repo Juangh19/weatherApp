682fc72df70702c140e44f05bb05fd11a3336dc4
Juan Garcia <juangarcia7337@gmail.com>
2023-07-30 14:40:11 -0500

    updating github pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: 'https://Juangh19.github.io/weatherApp',
+	base: '/weatherApp/',
 });
